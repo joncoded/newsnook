139d198cee1e8e36e177b2353a51d60ca7993343
joncoded <joncoded@gmail.com>
2026-02-23 10:48:29 -0500

    adjusting footer layout

--- components/tail.tsx
+++ components/tail.tsx
@@ -3,17 +3,16 @@ import { text } from "./text"
 
 export default function Tail () {
   return (
-    <footer className="p-5 py-16 bg-gradient-to-b from-gray-800 to-gray-900 w-full bottom-0">
-      <div className="w-screen-xl max-w-screen-xl mx-auto text-white text-center md:text-left flex flex-col md:flex-row items-center md:items-start justify-between gap-5">
-        <div className="tail-1">
-          <p className="mb-2"> ⚠️ articles appear via the <Link href="https://news.ycombinator.com/" target="_blank">ycombinator.com</Link> API - none are fully endorsed by me!</p>
-          <p> this is a <Link href="https://jonchius.com" target="_blank">@joncoded (aka @jonchius)</Link> <span aria-hidden="true">🐼🧑🏻‍💻</span> project </p>          
+    <footer className="p-5 py-16 bg-gradient-to-b from-gray-800 to-gray-900 w-full bottom-0 text-center">
+      <div className="w-screen-xl max-w-screen-xl mx-auto text-white flex-col md:flex-row items-center justify-between gap-10">
+        <div className="tail-1 mb-5">          
+          <p> a <a href="https://joncoded.com" className="text-green-400 hover:text-white" target="_blank">@joncoded</a> (aka <a href="https://jonchius.com" className="text-green-400 hover:text-white" target="_blank">@jonchius</a>) <span aria-hidden="true">🐼🧑🏻‍💻</span> project </p>          
         </div>
-        <div className="tail-2 flex gap-5 text-center">
+        <div className="tail-2 flex gap-5 justify-center">
           
-          <Link href="/about">{text['about']}</Link>
-          <Link href="/omni">{text['search']}</Link>
-          <Link href="/conf">{text['configuration']}</Link>
+          <Link className="text-green-400 hover:text-white" href="/about">{text['about']}</Link>
+          <Link className="text-green-400 hover:text-white" href="/omni">{text['search']}</Link>
+          <Link className="text-green-400 hover:text-white" href="/conf">{text['configuration']}</Link>
         </div>
       </div>
     </footer>
